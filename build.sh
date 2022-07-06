fbce592144ab9eccd48b7e71278ed9aabc8f493a
Blake Regalia <blake.regalia@gmail.com>
2022-07-05 19:41:45 -0700

    dev: init

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+mkdir -p proto
+
+function copy() {
+	rsync -a submodules/$1/ proto
+}
+
+copy wasmd/proto proto
+copy wasmd/third_party/proto proto
+# copy googleapis
+# copy protobuf
+# copy cosmos-proto/proto
+# copy cosmos-sdk/proto
+
+
+PROTO_PATH=""
+function push() {
+	PROTO_PATH="$PROTO_PATH${PROTO_PATH:+/}$1"
+}
+
+function pop() {
+	PROTO_PATH="${PROTO_PATH%/*}"
+}
+
+function gen_proto() {
+	file="$1"
+	echo "building $file..."
+	protoc -I=proto \
+		--ts_out=lib \
+		--ts_opt=explicit_override=true \
+		--ts_opt=no_namespace \
+		"$file"
+	script_name="${file%.proto}"
+	export_name="${script_name//\//_}"
+	echo "export * as $export_name from './$script_name';" >> lib/main.ts
+}
+
+function gen_all() {
+	find "$1" -type f -name *.proto -exec bash -c "gen_proto \"{}\"" \;
+}
+
+mkdir -p lib
+: > lib/main.ts
+
+export -f gen_proto
+
+gen_all proto/cosmos
+gen_all proto/cosmwasm
+
+tsc
