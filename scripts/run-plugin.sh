703d56be0bb55515c8b2896c4aa42aeb53695b6a
Blake Regalia <blake.regalia@gmail.com>
2024-05-03 20:17:18 -0700

    feat: add fiattokenfactory

--- scripts/run-plugin.sh
+++ scripts/run-plugin.sh
@@ -21,7 +21,7 @@ srd_dist="$srd_build/dist"
 
 
 # dirs to target proto files
-srd_chains=($srd_proto/{tendermint,cometbft,cosmos,ibc,cosmwasm,secret,akash,axelar,gaia,juno,osmosis,circle})
+srd_chains=($srd_proto/{tendermint,cometbft,cosmos,ibc,cosmwasm,secret,akash,axelar,gaia,juno,osmosis,circle,fiattokenfactory})
 
 # subdir to annotations to generate
 srd_annotations="$srd_gen/annotations"
